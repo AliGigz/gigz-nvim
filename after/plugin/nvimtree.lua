06a0dc72e2ca4de2caa098754cbae11a6bae5ec4
Ali <ali.gikzg@gmail.com>
2024-10-09 15:08:24 +0330

    Increased NvimTree width

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -10,16 +10,16 @@ require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 40,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
